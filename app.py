dbd2f8d3033aeafb98654e8a462580d25adbb231
paulo sacilotti <paulo.sacilotti@gmail.com>
2023-01-28 15:38:09 +0100

    tot

--- app.py
+++ app.py
@@ -7,26 +7,10 @@ import pytz
 from datetime import datetime
 
 '''
-# TaxiFareModel front
+UberPred - Prédire le prix d'une course 🚕
+Bienvenue, entre ton adresse de départ et ton adresse d'arrivée, tu verras combien ça te coûte 💸
 '''
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
-
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
 
 
 default_latitude = 40.730610
@@ -38,32 +22,14 @@ dropoff_longitude = st.number_input("Drop off longitude:", value=default_longitu
 dropoff_latitude = st.number_input("Drop off latitude:", value=default_latitude)
 
 passenger_count = st.number_input("number of passengers", value=1)
-'''
 
-## Once we have these, let's call our API in order to retrieve a prediction
-S
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
 
 
 url = 'https://taxifare.lewagon.ai/predict'
 
 if url == 'https://taxifare.lewagon.ai/predict':
 
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
 
-'''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
 key = "2013-07-06 17:18:00.000000119"
     # localize the user provided datetime with the NYC timezone
 #eastern = pytz.timezone("US/Eastern")
